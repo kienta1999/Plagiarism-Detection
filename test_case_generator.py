fbd10a16c54b575dc5d291bdbda980691b304b89
Kien Ta <talekien1710@gmail.com>
2022-04-25 22:55:22 -0500

    generate json recording where text come from

--- test_case_generator.py
+++ test_case_generator.py
@@ -1,12 +1,13 @@
 from constant import *
 import os
 import random
+import json
 
 random.seed(15)
 
 def get_random_database_file():
     database_file = random.choice(os.listdir(PATH_TO_SAMPLE_DATABASE))
-    return os.path.join(PATH_TO_SAMPLE_DATABASE, database_file)
+    return database_file
 
 # can mutate the sentence here
 def get_random_text(database_file, num_sentence):
@@ -21,25 +22,35 @@ def mutate_origin_pattern(original_pattern_file):
     original_pattern = f.read().split('.')
     f.close()
     # insert from 5 different file in database
+    chosen_files = []
     for _ in range(NUM_DB_COPIED_TO_PATTERN):
         database_file = get_random_database_file()
+        database_file_path = os.path.join(PATH_TO_SAMPLE_DATABASE, database_file)
         num_sentence = random.randint(1, MAX_NUM_SENTENCE)
-        text_from_db = get_random_text(database_file, num_sentence)
+        text_from_db = get_random_text(database_file_path, num_sentence)
+        chosen_files.append({"file": database_file, "num_sentence": len(text_from_db)})
         index_original_pattern = random.randint(0, len(original_pattern))
         # insert a random portion from db to the original pattern
         original_pattern[index_original_pattern:index_original_pattern] = text_from_db
-    return original_pattern
+    return original_pattern, chosen_files
 
 if __name__=='__main__':
     for path in (PATH_TO_SAMPLE_DATABASE, PATH_TO_SAMPLE_ORIGINAL_PATTERN, PATH_TO_SAMPLE_MODIFIED_PATTERN):
         if not os.path.exists(path):
             os.makedirs(path)
+    records = []
     for original_pattern_file in os.listdir(PATH_TO_SAMPLE_ORIGINAL_PATTERN):
+        record = {"name": original_pattern_file}
         original_pattern_file_path = os.path.join(PATH_TO_SAMPLE_ORIGINAL_PATTERN, original_pattern_file)
-        muatated_pattern = mutate_origin_pattern(original_pattern_file_path)
+        muatated_pattern, chosen_files = mutate_origin_pattern(original_pattern_file_path)
         muatated_pattern_file_path = os.path.join(PATH_TO_SAMPLE_MODIFIED_PATTERN, original_pattern_file)
         f = open(muatated_pattern_file_path, 'w')
         f.write('.'.join(muatated_pattern))
         f.close()
+        record["copy_from"] = chosen_files
+        records.append(record)
+    json_file = open(os.path.join(PATH_TO_SAMPLE_MODIFIED_PATTERN, "data.json"), 'w')
+    json.dump(records, json_file)
+    json_file.close()
 
         
