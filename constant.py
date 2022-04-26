ea8c02be8a8b754190b480efd9ff35a744972925
Kien Ta <talekien1710@gmail.com>
2022-04-25 22:45:24 -0500

    code to generate test case for pattern created

--- constant.py
+++ constant.py
@@ -1,7 +1,11 @@
 NUM_SHINGLES = 2
 
+NUM_DB_COPIED_TO_PATTERN = 5
+
+MAX_NUM_SENTENCE = 5
+
 PATH_TO_SAMPLE_DATABASE = './data_sample/raw_news_database'
 
-PATH_TO_ORIGINAL_PATTERN = './data_sample/raw_news_original_pattern'
+PATH_TO_SAMPLE_ORIGINAL_PATTERN = './data_sample/raw_news_original_pattern'
 
-PATH_TO_MODIFIED_PATTERN = './data_sample/raw_news_modified_pattern'
+PATH_TO_SAMPLE_MODIFIED_PATTERN = './data_sample/raw_news_modified_pattern'
--- test_case_generator.py
+++ test_case_generator.py
@@ -1,6 +1,45 @@
-from constant import PATH_TO_SAMPLE_DATABASE, PATH_TO_ORIGINAL_PATTERN, PATH_TO_MODIFIED_PATTERN
+from constant import *
 import os
+import random
 
-for path in (PATH_TO_SAMPLE_DATABASE, PATH_TO_ORIGINAL_PATTERN, PATH_TO_MODIFIED_PATTERN):
-    if not os.path.exists(path):
-        os.makedirs(path)
+random.seed(15)
+
+def get_random_database_file():
+    database_file = random.choice(os.listdir(PATH_TO_SAMPLE_DATABASE))
+    return os.path.join(PATH_TO_SAMPLE_DATABASE, database_file)
+
+# can mutate the sentence here
+def get_random_text(database_file, num_sentence):
+    f = open(database_file, 'r')
+    content = f.read().split('.')
+    index = random.randint(0, max(len(content) - num_sentence, 0))
+    f.close()
+    return content[index:index+num_sentence]
+
+def mutate_origin_pattern(original_pattern_file):
+    f = open(original_pattern_file, 'r')
+    original_pattern = f.read().split('.')
+    f.close()
+    # insert from 5 different file in database
+    for _ in range(NUM_DB_COPIED_TO_PATTERN):
+        database_file = get_random_database_file()
+        num_sentence = random.randint(1, MAX_NUM_SENTENCE)
+        text_from_db = get_random_text(database_file, num_sentence)
+        index_original_pattern = random.randint(0, len(original_pattern))
+        # insert a random portion from db to the original pattern
+        original_pattern[index_original_pattern:index_original_pattern] = text_from_db
+    return original_pattern
+
+if __name__=='__main__':
+    for path in (PATH_TO_SAMPLE_DATABASE, PATH_TO_SAMPLE_ORIGINAL_PATTERN, PATH_TO_SAMPLE_MODIFIED_PATTERN):
+        if not os.path.exists(path):
+            os.makedirs(path)
+    for original_pattern_file in os.listdir(PATH_TO_SAMPLE_ORIGINAL_PATTERN):
+        original_pattern_file_path = os.path.join(PATH_TO_SAMPLE_ORIGINAL_PATTERN, original_pattern_file)
+        muatated_pattern = mutate_origin_pattern(original_pattern_file_path)
+        muatated_pattern_file_path = os.path.join(PATH_TO_SAMPLE_MODIFIED_PATTERN, original_pattern_file)
+        f = open(muatated_pattern_file_path, 'w')
+        f.write('.'.join(muatated_pattern))
+        f.close()
+
+        
