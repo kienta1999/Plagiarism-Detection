20cd8e57996f2893f12e0f61d0b68ac1b064682e
Kien Ta <talekien1710@gmail.com>
2022-04-30 00:32:17 -0500

    add nltk downloader

--- word_preprocessing.py
+++ word_preprocessing.py
@@ -5,6 +5,7 @@ import nltk
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('punkt')
+nltk.download('omw-1.4')
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
